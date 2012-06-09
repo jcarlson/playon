02c1bf8a16ff86a36bb71582aba3265726e5e8b9
Jarrod Carlson <jarrod.carlson@gmail.com>
2012-06-09 11:57:24 -0400

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+afl     = Publisher.create :name => "Arena Football League"
+cobb    = Publisher.create :name => "Cobb Little League"
+playon  = Publisher.create :name => "PlayOn! Productions"
+
+Event.create :name => "Jacksonville Sharks at Georgia Force", :scheduled_date => "09 Jun 2012, 7:00 PM ET", :publisher_id => afl.id
+
+Event.create :name => "Little League World Series, Game 1", :scheduled_date => "28 June 2012, 12:00 PM ET", :publisher_id => cobb.id
+
+Event.create :name => "Friday Night Football", :scheduled_date => "17 Aug 2012, 7:00 PM ET", :publisher_id => playon.id
