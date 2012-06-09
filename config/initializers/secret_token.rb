7c5b53a6384d1a33a51b7815b3231846142e1500
Jarrod Carlson <jarrod.carlson@gmail.com>
2012-06-09 11:07:01 -0400

    initial code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playon::Application.config.secret_token = '4bee89af95c2af69017dc1db88990dfe15a88e1a7492e2032388a103acd8cf4560ee60d739583e8ae8da910783d41ab91cadce18eedd583278ab958428346977'
