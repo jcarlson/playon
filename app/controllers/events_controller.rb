7a77223edb7fe75f46d91a8aea32c8cfa8ccb3c4
Jarrod Carlson <jarrod.carlson@gmail.com>
2012-06-13 22:40:12 -0400

    Fill in some CRUD operations to round out the reference implementation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,36 @@
 class EventsController < ApplicationController
+  
+  respond_to :html, :json
+  
   def index
-    @events = Event.scoped
+    respond_with(@events = Event.all)
   end
+  
+  def new
+    respond_with(@event = Event.new)
+  end
+  
+  def create
+    respond_with(@event = Event.create(params[:event]))
+  end
+  
+  def show
+    respond_with(@event = Event.find(params[:id]))
+  end
+  
+  def edit
+    respond_with(@event = Event.find(params[:id]))
+  end
+  
+  def update
+    @event = Event.update(params[:id], params[:event])
+    flash[:notice] = "Event updated"
+    respond_with(@event)
+  end
+  
+  def destroy
+    flash[:notice] = "Event deleted (not really, though)"
+    redirect_to events_path
+  end
+  
 end
