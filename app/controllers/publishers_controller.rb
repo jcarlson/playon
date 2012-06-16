28ab553dd5dd0a2ffbb5a6e2fff737e2eabe4175
Jarrod Carlson <jarrod.carlson@gmail.com>
2012-06-16 12:18:05 -0400

    Publishers JSON controller and views

---
+++ app/controllers/publishers_controller.rb
@@ -0,0 +1,36 @@
+class PublishersController < ApplicationController
+  
+  respond_to :json
+  
+  def index
+    respond_with(@publishers = Publisher.all)
+  end
+  
+  def new
+    respond_with(@publisher = Publisher.new)
+  end
+  
+  def create
+    respond_with(@publisher = Publisher.create(params[:publisher]))
+  end
+  
+  def show
+    respond_with(@publisher = Publisher.find(params[:id]))
+  end
+  
+  def edit
+    respond_with(@publisher = Publisher.find(params[:id]))
+  end
+  
+  def update
+    @publisher = Publisher.update(params[:id], params[:publisher])
+    flash[:notice] = "Publisher updated"
+    respond_with(@publisher)
+  end
+  
+  def destroy
+    flash[:notice] = "Publisher deleted (not really, though)"
+    redirect_to publishers_path
+  end
+  
+end
